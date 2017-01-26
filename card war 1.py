5a8da53edfce16a58851f992e1336230687dd2ab
AhmadIsAwesome <pirza007.318@lgflmail.net>
2017-01-26 11:49:41 +0000

    Add files via upload

---
+++ card war 1.py
@@ -0,0 +1,8 @@
+import random
+
+name = input("Hello whats your name ? ")
+print("Welcome " +   name)
+decksize= input("How many cards do you want, 3 , 6, 10, 13: ")
+print("Ok , So you want to play with " + decksize,  "cards!")
+print("Do you want to playwith a friend or with the computer")
+input if  print ("Whats there name ?")
