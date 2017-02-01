f0bf1df24f44c43a86a8949636e2b59c0dccad02
AhmadIsAwesome <pirza007.318@lgflmail.net>
2017-02-01 14:28:49 +0000

    Update card war 1.py

--- card war 1.py
+++ card war 1.py
@@ -2,7 +2,15 @@ import random
 
 name = input("Hello whats your name ? ")
 print("Welcome " +   name)
-decksize= input("How many cards do you want, 3 , 6, 10, 13: ")
+decksize= input("How many cards do you want, 3 , 6, 10: ")
 print("Ok , So you want to play with " + decksize,  "cards!")
-print("Do you want to playwith a friend or with the computer")
-input if  print ("Whats there name ?")
+
+player = input("play with a friend or the computer ? [Friend : Computer]")
+
+if player == 'Friend':
+    player2 = input("What will his/her name be?:")
+    print("Hello! " + player2)
+
+elif player == 'Computer':
+    print("Be lonely then")
+
